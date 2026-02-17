84349940a6d0b3c8c2c37ab199805065dd7f6fd9
Danil <ryphentalrhino@mail.ru>
2026-02-17 18:50:18 +0700

    1.7. Класс постоянного назначения

---
+++ RBAC/src/main/java/org/example/PermanentAssignment.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class PermanentAssignment extends AbstractRoleAssignment {
+    private boolean revoked;
+
+    public PermanentAssignment(User user, Role role, AssignmentMetadata metadata) {
+        super(user, role, metadata);
+        this.revoked = false;
+    }
+
+    @Override
+    public boolean isActive() {
+        return !revoked;
+    }
+
+    @Override
+    public String assignmentType() {
+        return "PERMANENT";
+    }
+
+    public void revoke() {
+        this.revoked = true;
+    }
+
+    public boolean isRevoked() {
+        return revoked;
+    }
+
+    @Override
+    public String summary() {
+        return super.summary(); // будет использовать переопределенный isActive()
+    }
+}
